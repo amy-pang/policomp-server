d7733f7cf4b4dde5b6a1d8dc32072037d87ea385
Amy Pang <amypang@umich.edu>
2024-08-02 17:13:25 -0500

    Initial commit with only the intended folder

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Comparison() {
+    return (
+        <div>
+            {/* Add your content here */}
+        </div>
+    );
+}
+
+export default Comparison;
